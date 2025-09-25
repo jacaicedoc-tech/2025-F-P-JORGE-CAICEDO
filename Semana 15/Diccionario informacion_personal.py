20641abd215b42e99b172a8bc663491b47b00c01
Jorge Andres Caicedo Chere <ja.caicedoc@uea.edu.ec>
2025-09-24 21:58:00 -0500

    Diccionario informacion_personal

--- Semana 15/Diccionario informacion_personal.py
+++ Semana 15/Diccionario informacion_personal.py
@@ -1,21 +1,13 @@
-# Crear un Diccionario
-informacion_personal = {
-    "nombre": "Kiara Ortiz",
-    "edad": 33,
-    "ciudad": "Esmeraldas",
-    "profesion": "Sociologa"
-}
+# Crear un Diccionario con informacion personal
+informacion_personal = {"nombre": "Kiara Ortiz","edad": 33,"ciudad": "Quito","profesion": "Sociologa"}
 
-# Cambiar el valor de la clave "ciudad"
-informacion_personal["ciudad"] = "Quito"
-
-# Agregar (o modificar) la clave "profesion"
-informacion_personal["profesion"] = "Desarrollador de Software"
+informacion_personal["ciudad"] = "Esmeraldas"
+informacion_personal["profesion"] = "Abogada"
 
 if "telefono" not in informacion_personal:
-    informacion_personal["telefono"] = "0999999999"
+    informacion_personal["telefono"] = "0987654321"
 
-informacion_personal.pop("edad", None)  # None evita error si la clave no existe
+del(informacion_personal["edad"])
 
 print("Diccionario final:")
 print(informacion_personal)
